a8f15268009437c116709ef3f6b37ae5f6e0d542
vhlHORUS <87332736+vhlHORUS@users.noreply.github.com>
2021-07-12 14:59:03 -0300

    Arquivando, teste

---
+++ Apple/main.js
@@ -0,0 +1,132 @@
+<nav class="navbar navbar-expand-lg fixed-top">
+        
+        <div class="navconteudo-g">
+            <div class="navbar container-fluid navbar-apple some">
+                <a class="navbar-brand" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="44"
+                        viewBox="0 0 16 44">
+                        <path
+                            d="M8.02 16.23c-.73 0-1.86-.83-3.05-.8-1.57.02-3.01.91-3.82 2.32-1.63 2.83-.42 7.01 1.17 9.31.78 1.12 1.7 2.38 2.92 2.34 1.17-.05 1.61-.76 3.03-.76 1.41 0 1.81.76 3.05.73 1.26-.02 2.06-1.14 2.83-2.27.89-1.3 1.26-2.56 1.28-2.63-.03-.01-2.45-.94-2.48-3.74-.02-2.34 1.91-3.46 2-3.51-1.1-1.61-2.79-1.79-3.38-1.83-1.54-.12-2.83.84-3.55.84zm2.6-2.36c.65-.78 1.08-1.87.96-2.95-.93.04-2.05.62-2.72 1.4-.6.69-1.12 1.8-.98 2.86 1.03.08 2.09-.53 2.74-1.31"
+                            fill="#fff" /></svg></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">Mac</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">iPad</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">iPhone</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">Watch</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">Tv</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">Music</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="">Suporte</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"><svg xmlns="http://www.w3.org/2000/svg"
+                                    width="18" height="44" viewBox="0 0 18 44" style="fill:#ffffffb4;">
+                                    <path
+                                        d="M17.512,29.417l-4.786-4.788a7.068,7.068,0,1,0-.777.778l4.785,4.788a.55.55,0,1,0,.777-.777ZM1.427,20.054a5.946,5.946,0,1,1,5.946,5.954A5.957,5.957,0,0,1,1.427,20.054Z" />
+    
+                                </svg></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#"><svg xmlns="http://www.w3.org/2000/svg"
+                                    width="17" height="44" viewBox="0 0 17 44" style="fill:#ffffffab;">
+                                    <path
+                                        d="M14.5,14H12.712a4.227,4.227,0,0,0-8.424,0H2.5A2.5,2.5,0,0,0,0,16.5v11A2.5,2.5,0,0,0,2.5,30h12A2.5,2.5,0,0,0,17,27.5v-11A2.5,2.5,0,0,0,14.5,14Zm-6-2.875A3.243,3.243,0,0,1,11.712,14H5.288A3.243,3.243,0,0,1,8.5,11.125ZM16,27.5A1.5,1.5,0,0,1,14.5,29H2.5A1.5,1.5,0,0,1,1,27.5v-11A1.5,1.5,0,0,1,2.5,15h12A1.5,1.5,0,0,1,16,16.5Z" />
+                                </svg></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- navbar celular -->
+        <div class="navconteudo-p">
+            <div class="navbar navbar-cel">
+                <ul>
+                    <li><svg viewBox="0 0 100 80" width="25" height="30" style="fill:#ffffffab;">
+                            <rect y="25" width="70" height="8" rx="6"></rect>
+                            <rect y="50" width="70" height="8" rx="6"></rect>
+    
+                        </svg></li>
+                    <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="44" viewBox="0 0 16 44">
+                            <path
+                                d="M8.02 16.23c-.73 0-1.86-.83-3.05-.8-1.57.02-3.01.91-3.82 2.32-1.63 2.83-.42 7.01 1.17 9.31.78 1.12 1.7 2.38 2.92 2.34 1.17-.05 1.61-.76 3.03-.76 1.41 0 1.81.76 3.05.73 1.26-.02 2.06-1.14 2.83-2.27.89-1.3 1.26-2.56 1.28-2.63-.03-.01-2.45-.94-2.48-3.74-.02-2.34 1.91-3.46 2-3.51-1.1-1.61-2.79-1.79-3.38-1.83-1.54-.12-2.83.84-3.55.84zm2.6-2.36c.65-.78 1.08-1.87.96-2.95-.93.04-2.05.62-2.72 1.4-.6.69-1.12 1.8-.98 2.86 1.03.08 2.09-.53 2.74-1.31"
+                                fill="#ffffffb4" /></svg> </li>
+                    <li style="padding: 0 16px;">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="44" viewBox="0 0 17 44"
+                            style="fill:#ffffffab;">
+    
+                            <path
+                                d="M14.5,14H12.712a4.227,4.227,0,0,0-8.424,0H2.5A2.5,2.5,0,0,0,0,16.5v11A2.5,2.5,0,0,0,2.5,30h12A2.5,2.5,0,0,0,17,27.5v-11A2.5,2.5,0,0,0,14.5,14Zm-6-2.875A3.243,3.243,0,0,1,11.712,14H5.288A3.243,3.243,0,0,1,8.5,11.125ZM16,27.5A1.5,1.5,0,0,1,14.5,29H2.5A1.5,1.5,0,0,1,1,27.5v-11A1.5,1.5,0,0,1,2.5,15h12A1.5,1.5,0,0,1,16,16.5Z" />
+    
+                        </svg>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+// css
+
+.navbar {
+    height: 44px;
+    width: 100%;
+
+    background-color: #333333;
+}
+
+.navbar-apple {
+    margin-left: 279.500px;
+    margin-right: 279.500px;
+
+    padding-left: 22px;
+    padding-right: 22px;
+}
+
+.navbar-apple li {
+    margin-left: 57.5px;
+}
+
+.navbar a {
+    color: rgba(255, 255, 255, 0.616);
+}
+
+.navbar a:hover {
+    color: white;
+}
+
+.navbar ul {
+    display: flex;
+
+    justify-content: center;
+
+    align-items: center;
+}
+
+.navbar-cel {
+    display: none;
+}
